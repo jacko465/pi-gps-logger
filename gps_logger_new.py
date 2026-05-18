f1d39f28145fae8513b02e8eed3ed7f8c8d77171
jacko465 <jack.barker465@gmail.com>
2026-05-18 17:07:17 +1000

    added new gps read class for testing

--- gps_logger_new.py
+++ gps_logger_new.py
@@ -1,5 +1,6 @@
 import pynmea2
 import serial
+import io
 
 class GpsSerial:
     def __init__(self):
@@ -9,10 +10,31 @@ class GpsSerial:
         self.debug = False
 
         self.serial = None
+        self.sio = None
 
     def open_serial(self):
         print("Opening serial port...")
-        
+        self.serial = serial.Serial(self.serial_port, baudrate=self.baudrate, timeout=1)
+        self.sio = io.TextIOWrapper(io.BufferedRWPair(self.serial, self.serial), encoding=self.serial_encoding)
+
+    def close_serial(self):
+        if self.serial:
+            self.serial.close()
+            print('serial port closed')
 
 if __name__ == '__main__':
-    pass
+    gps_serial = GpsSerial()
+    try:
+        gps_serial.open_serial()
+        print("Waiting for GPS data...")
+        while True:
+            try:
+                line = gps_serial.sio.readline()
+                print(line)
+            except serial.SerialException as e:
+                print(f'Device error: {e}')
+                break
+    except Exception as e:
+        print(f'Exception: {e}')
+    finally:
+        gps_serial.close_serial()
