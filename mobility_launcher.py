a33f7262954b2e126cfe1a3575409f41574045af
jacko465 <jack.barker465@gmail.com>
2026-05-25 11:35:58 +1000

    added blank launcher

---
+++ mobility_launcher.py
@@ -0,0 +1,8 @@
+'''
+Mobility Launcher
+This script serves as a main entry point for launching the gps logger
+when using the adafruit tft display and buttons for user interaction.
+Button functions:
+    - Start GPS logging
+    - Stop and save gps log to csv
+'''
